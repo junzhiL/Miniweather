c66044ccb56d8093483aa4dfd567de31b0cc5ec6
junzhiL <843334966@qq.com>
2018-11-21 18:58:58 +0800

    静态天气页面

---
+++ app/src/main/java/cn/edu/pku/liujunzhi/miniweather/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.pku.liujunzhi.miniweather;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
